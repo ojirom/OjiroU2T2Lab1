77feeb1dc57da4a737a94d0e61078fb020769bd0
BT_1N3_22 <sample@bths.edu>
2023-09-28 10:57:28 -0400

    done

---
+++ src/Point.java
@@ -0,0 +1,47 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Point(int x) {
+        this.x = x;
+        y = x;
+    }
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public void setX(int newX) {
+        x = newX;
+    }
+    public void setY(int newY) {
+        y = newY;
+    }
+    public String coordinate() {
+        return "(" + x + ", " + y + ")";
+    }
+    public String quadrant() {
+        if (x > 0 && y > 0) {
+            return "I";
+        } else if (x < 0 && y > 0) {
+            return "II";
+        } else if (x < 0 && y < 0) {
+            return "III";
+        } else if (x > 0 && y < 0) {
+            return "IV";
+        } else if (x == 0 && y == 0) {
+            return "origin";
+        } else {
+            return "on an axis";
+        }
+    }
+}
